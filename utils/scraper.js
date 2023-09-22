a86d5eb91542da2e6f7a1f98c6bf7450185e8583
NAME <mail@gmail.com>
2023-09-23 00:24:15 +0530

    converted all json property to camelCase

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,20 +1,21 @@
 import PDFParser from "pdf2json";
+import camelCase from "camelcase";
 import parseExperience from "./parseExperience.js";
 import parseEducation from "./parseEducation.js";
 
 const scraper = (pdfBuffer) => {
   return new Promise((resolve, reject) => {
-    try {
-      const pdfParser = new PDFParser();
-      pdfParser.parseBuffer(pdfBuffer);
-     // pdfParser.loadPDF("/storage/emulated/0/Download/Steely_M.pdf");
+    const pdfParser = new PDFParser();
+    pdfParser.parseBuffer(pdfBuffer);
+    // pdfParser.loadPDF("/storage/emulated/0/Download/Steely_M.pdf");
 
-      pdfParser.on("pdfParser_dataError", (errData) => {
-        console.error(errData.parserError);
-        reject(new Error(errData));
-      });
+    pdfParser.on("pdfParser_dataError", (errData) => {
+      console.error(errData.parserError);
+      reject(new Error(errData));
+    });
 
-      pdfParser.on("pdfParser_dataReady", (pdfData) => {
+    pdfParser.on("pdfParser_dataReady", (pdfData) => {
+      try {
         //converting URI encoding to normal text
         for (var i = 0; i < pdfData.Pages.length; i++) {
           for (var j = 0; j < pdfData.Pages[i].Texts.length; j++) {
@@ -48,28 +49,28 @@ const scraper = (pdfBuffer) => {
           //console.log(leftTexts[i]);
           if (leftTexts[i].R[0].TS[1] === 16) {
             leftActiveSection = leftTexts[i].R[0].T;
-            leftData[leftActiveSection] = [];
+            leftData[camelCase(leftActiveSection)] = [];
           } else {
             //console.log(leftTexts[i]);
             if (
-              leftData[leftActiveSection].length === 0 ||
+              leftData[camelCase(leftActiveSection)].length === 0 ||
               leftTexts[i - 1].oc === "#a8b0b5" ||
               leftTexts[i].y - leftTexts[i - 1]?.y > 1
             ) {
-              leftData[leftActiveSection].push(leftTexts[i].R[0].T);
+              leftData[camelCase(leftActiveSection)].push(leftTexts[i].R[0].T);
             } else {
-              leftData[leftActiveSection][
-                leftData[leftActiveSection].length - 1
+              leftData[camelCase(leftActiveSection)][
+                leftData[camelCase(leftActiveSection)].length - 1
               ] +=
                 isValidUrl(
-                  leftData[leftActiveSection][
-                    leftData[leftActiveSection].length - 1
+                  leftData[camelCase(leftActiveSection)][
+                    leftData[camelCase(leftActiveSection)].length - 1
                   ]
                 ) && leftTexts[i].oc !== "#a8b0b5"
                   ? ""
                   : " ";
-              leftData[leftActiveSection][
-                leftData[leftActiveSection].length - 1
+              leftData[camelCase(leftActiveSection)][
+                leftData[camelCase(leftActiveSection)].length - 1
               ] += leftTexts[i].R[0].T.trim();
             }
           }
@@ -109,33 +110,33 @@ const scraper = (pdfBuffer) => {
         }
 
         summaryRelatedTexts.forEach((item, i) => {
-          if (!rightData.Summary) {
-            rightData.Summary = "";
+          if (!rightData.summary) {
+            rightData.summary = "";
           }
           // console.log(item);
           if (item.y - summaryRelatedTexts[i - 1]?.y > 1.8) {
-            rightData.Summary += "\n\n";
+            rightData.summary += "\n\n";
           } else if (
             item.y - summaryRelatedTexts[i - 1]?.y > 1.2 ||
             /^-|^•|^ /.test(item.R[0].T)
           ) {
-            rightData.Summary += "\n";
+            rightData.summary += "\n";
           } else {
-            rightData.Summary += " ";
+            rightData.summary += " ";
           }
-          rightData.Summary += item.R[0].T;
-          rightData.Summary = rightData.Summary.trim();
+          rightData.summary += item.R[0].T;
+          rightData.summary = rightData.summary.trim();
         });
 
-        rightData.Experience = experience;
-        rightData.Education = education;
+        rightData.experience = experience;
+        rightData.education = education;
 
         const finalData = { ...rightData, ...leftData };
         resolve(finalData);
-      });
-    } catch (error) {
-      reject(new Error(error));
-    }
+      } catch (error) {
+        reject(new Error(error));
+      }
+    });
   });
 };
 function isValidUrl(str) {
