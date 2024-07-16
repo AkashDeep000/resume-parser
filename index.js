bd428fa73b86c1773c0aee88b55321536b9ec67c
AkashDeep000 <akashdeepdas970@gmail.com>
2024-07-16 14:59:33 +0530

    added log

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get('/profile', async (req, res) => {
   const csrfToken = process.env.CSRFTOKEN
   log(cookie, csrfToken)
   const profileUrl = req.query.url
+  log(profileUrl)
   const test = await fetch("https://geo.brdtest.com/mygeo.json", {agent})
   log(await test.json())
   const htmlRes = await fetch(profileUrl, {
