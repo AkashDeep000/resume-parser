bb0bec393d4e1446bc48a7025ec75ad30371ce3d
AkashDeep000 <akashdeepdas970@gmail.com>
2024-07-16 11:08:25 +0530

    added direct profile scraper from url

--- index.js
+++ index.js
@@ -2,34 +2,130 @@ import fs from "fs/promises";
 import express from "express";
 import fileUpload from "express-fileupload";
 import scraper from "./utils/scraper.js";
+import dotenv from "dotenv"
+import { log } from "console";
 
+dotenv.config()
 const app = express();
 app.use(fileUpload());
-const port = 3000;
+const port = 3001;
+
+app.get('/profile', async (req, res) => {
+  const cookie = process.env.COOKIE
+  const csrfToken = process.env.CSRFTOKEN
+
+  const profileUrl = req.query.url
+  const htmlRes = await fetch(profileUrl, {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,bn;q=0.7",
+      "cache-control": "no-cache",
+      "pragma": "no-cache",
+      "priority": "u=0, i",
+      "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Linux\"",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "cookie": cookie
+    },
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET"
+  });
+
+  const html = await htmlRes.text()
+
+  const profileId = html.split("urn:li:fsd_profileCard:(")[1].split(",")[0]
+
+  const reqs = await fetch("https://www.linkedin.com/voyager/api/graphql?action=execute&queryId=voyagerIdentityDashProfileActionsV2.ca80b3b293240baf5a00226d8d6d78a1", {
+    "headers": {
+      "accept": "application/vnd.linkedin.normalized+json+2.1",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,bn;q=0.7",
+      "cache-control": "no-cache",
+      "content-type": "application/json; charset=UTF-8",
+      "csrf-token": csrfToken,
+      "pragma": "no-cache",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Linux\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-li-lang": "en_US",
+      "x-li-track": "{\"clientVersion\":\"1.13.19864\",\"mpVersion\":\"1.13.19864\",\"osName\":\"web\",\"timezoneOffset\":5.5,\"timezone\":\"Asia/Calcutta\",\"deviceFormFactor\":\"DESKTOP\",\"mpName\":\"voyager-web\",\"displayDensity\":1.25,\"displayWidth\":1920,\"displayHeight\":1200}",
+      "x-restli-protocol-version": "2.0.0",
+      "cookie": cookie,
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": `{\"variables\":{\"profileUrn\":\"urn:li:fsd_profile:${profileId}\"},\"queryId\":\"voyagerIdentityDashProfileActionsV2.ca80b3b293240baf5a00226d8d6d78a1\",\"includeWebMetadata\":true}`,
+    "method": "POST"
+  });
+
+  if (reqs.status !== 200) {
+    const data = {
+      success: false,
+    }
+    return res.status(400).json(data)
+  }
+
+  const data = await reqs.json()
+  // @ts-ignore
+  const pdfLink = data.data.data.doSaveToPdfV2IdentityDashProfileActionsV2.result.downloadUrl
+  const pdfRes = await fetch(pdfLink, {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,bn;q=0.7",
+      "cache-control": "no-cache",
+      "pragma": "no-cache",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Linux\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "cookie": cookie,
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": null,
+    "method": "GET"
+  });
+  //console.log(pdfRes);
+
+  const pdfFile = await pdfRes.arrayBuffer()
+  const pdfData = await scraper(pdfFile);
+  return res.json(pdfData);
+
+})
 
 app.all("/", async (req, res) => {
   try {
-  const demo = req.query.demo;
-  if (!(req.files?.pdf || demo)) {
-    return res
-      .status(400)
-      .send(
-        "No PDF files is provided.\n\nView demo by adding '?demo=1' at the end of the url"
-      );
-  }
-
-  let pdfBuffer;
-  if (demo) {
-    if (demo > 10 || demo < 1) {
+    const demo = req.query.demo;
+    if (!(req.files?.pdf || demo)) {
       return res
         .status(400)
-        .send("Only 10 demo available (must be between 1 to 10)");
+        .send(
+          "No PDF files is provided.\n\nView demo by adding '?demo=1' at the end of the url"
+        );
     }
-    pdfBuffer = await fs.readFile(`cv/${demo}.pdf`);
-  } else {
-    pdfBuffer = req.files.pdf.data;
-  }
-  
+
+    let pdfBuffer;
+    if (demo) {
+      if (demo > 10 || demo < 1) {
+        return res
+          .status(400)
+          .send("Only 10 demo available (must be between 1 to 10)");
+      }
+      pdfBuffer = await fs.readFile(`cv/${demo}.pdf`);
+    } else {
+      pdfBuffer = req.files.pdf.data;
+    }
+
     const data = await scraper(pdfBuffer);
     return res.json(data);
   } catch (error) {
